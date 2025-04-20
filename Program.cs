1a6a226c9ed17c4d13a8fd00dda08843533092e3
Julianparra <julianparra1230@gmail.com>
2025-04-20 19:25:49 -0300

    Primeros pasos C#

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+/* tipos de datos:
+ * Int Numeros enteros
+ * Float Numeros Decimales
+ * Bool Verdadero o falso
+ * String textos
+ * var (no es buena practica)
+ * char caracter $
+ * Declarar una variable (Tipo de dato, nombre, y valor)
+ */
+//declarar una variable ejemplo: int edad = 23;
+
+string nombre;
+
+Console.WriteLine("¿Cual es tu nombre?");
+nombre = Console.ReadLine();
+
+Console.Clear(); // limpia la consola
+
+int edad;
+
+Console.WriteLine("¿Cuantos años tienes?");
+edad = int.Parse(Console.ReadLine()); // Int.parse me permite cambiar el tipo de dato en este caso de string a entero (int)
+
+Console.WriteLine("Jugador " + nombre + ".");
+Console.WriteLine("tiene " + edad + " años.");
