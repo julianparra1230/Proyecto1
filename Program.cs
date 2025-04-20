80f12b7158ede9bd674668dc077e7857b61e12c7
Julianparra <julianparra1230@gmail.com>
2025-04-20 19:28:28 -0300

    Primeros pasos C#V2

--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-/* tipos de datos:
- * Int Numeros enteros
- * Float Numeros Decimales
- * Bool Verdadero o falso
- * String textos
- * var (no es buena practica)
- * char caracter $
- * Declarar una variable (Tipo de dato, nombre, y valor)
- */
+ /* tipos de datos:
+  * Int Numeros enteros
+  * Float Numeros Decimales
+  * Bool Verdadero o falso
+  * String textos
+  * var (no es buena practica)
+  * char caracter $
+  * Declarar una variable (Tipo de dato, nombre, y valor)
+  */ // Const deja de ser variable para no poderse modificar, en ves de un tipo de dato se coloca const
 //declarar una variable ejemplo: int edad = 23;
 
 string nombre;
