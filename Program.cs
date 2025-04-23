c0c7f961d32ba925db920085024acb87c843f539
Julianparra <julianparra1230@gmail.com>
2025-04-22 21:27:39 -0300

    Mi primer juego de adivinar el numero con bucles

--- Program.cs
+++ Program.cs
@@ -25,3 +25,38 @@ edad = int.Parse(Console.ReadLine()); // Int.parse me permite cambiar el tipo de
 
 Console.WriteLine("Jugador " + nombre + ".");
 Console.WriteLine("tiene " + edad + " años.");
+Console.Clear();
+
+//Vamos a hacer un adivina el numero
+//Generar un numero aleatorio entre 1 y 100
+Random rdm = new Random();
+int numeroOculto = rdm.Next(1, 101); // el 101 no se incluye
+int intentos = 0;
+int numeroJugador = 0;
+int intentosMaximos = 5;
+Console.WriteLine($"{nombre}, Adivina el numero!!");
+Console.WriteLine("Entre el 1 y el 100");
+Console.WriteLine($"tienes {intentosMaximos} intentos para adivinar el numero!!");
+do
+{
+   numeroJugador = int.Parse(Console.ReadLine());
+    intentos++;
+
+    if (numeroJugador < numeroOculto)
+        Console.WriteLine("El Numero es mas Alto!");
+    else if (numeroJugador > numeroOculto)
+        Console.WriteLine("El numero es mas Bajo!");
+    else
+    { 
+        Console.WriteLine($"Felicitaciones {nombre}, Adivinaste el numero en " + intentos + " intentos");
+    break; //si adivina termina el juego
+    }
+
+    if (intentos == intentosMaximos && numeroJugador != numeroOculto)
+    {
+    Console.WriteLine($"Perdisteeee {nombre}, el numero era {numeroOculto}");
+    }
+}
+while (intentos < intentosMaximos && numeroJugador != numeroOculto);
+
+// De esta forma realizando un bucle y con Random generamos un juego de adivina el numero.
